d5871e9e8b3137b13c21502dfe64e460ceb06110
Juan Carlos Chang <juancarloschang@SINT-SDTI-LP2.munipanama.gob.pa>
2020-01-03 14:36:52 -0500

    Ínitial commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+//make sure service workers are supported
+if('serviceWorker' in navigator) {
+    window.addEventListener('load',() => {
+        navigator.serviceWorker
+        .register('../sw_cached_pages.js')
+        .then(reg => console.log('Service worker :Registered'))
+        .catch(err => console.log(`Service Worker: Error: ${err}`))
+
+    });
+    
+
+}
