d891df699bc3f074c858ee80a5ad10878704257f
OnekChangS <52790919+OnekChangS@users.noreply.github.com>
2020-01-16 14:36:48 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ if('serviceWorker' in navigator) {
         navigator.serviceWorker
         .register('../sw_cached_pages.js')
         .then(reg => console.log('Service worker :Registered'))
-        .catch(err => console.log(`Service Worker: Error: ${err}`))
+        .catch(err => console.log(`Service worker: Error: ${err}`))
 
     });
     
